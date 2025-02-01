b8e983fa0a3acd68f553e5b1b6cee96406546ffa
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-02-01 13:45:12 +0100

    ✨ Ajout de la liste des technologies utilisées dans les projets. ⚙️ Création des fichiers de données pour chaque technologie.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Technology } from '@models/technology.model';
+import { TechnologyService } from '@services/technology.service';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  technologies: Technology[] = [];
 
+  constructor(private technologyService: TechnologyService) {}
+
+  ngOnInit(): void {
+    this.technologies = this.technologyService.getTechnologies();
+  }
 }
---
+++ src/app/services/technology.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { Technology } from '../models/technology.model';
+import { Technologies } from '@data/tech/index';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TechnologyService {
+  constructor() {}
+
+  getTechnologies(): Technology[] {
+    return Technologies;
+  }
+
+  getTechnology(name: string): Technology | undefined {
+    return Technologies.find((tech) => tech.name === name);
+  }
+}
