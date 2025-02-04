1f5bd4aa91b5b1e6e271c4eda9380539db0ffe53
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-02-04 11:06:48 +0100

    ✨ Ajout de la sélection et déseléction des technologies.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,10 +11,29 @@ import { TechnologyService } from '@services/technology.service';
 })
 export class ProjectsComponent implements OnInit {
   technologies: Technology[] = [];
+  selectedTechnologies: Technology[] = [];
 
   constructor(private technologyService: TechnologyService) {}
 
   ngOnInit(): void {
     this.technologies = this.technologyService.getTechnologies();
+  }
+
+  toggleTech(technology: Technology): void {
+    if (this.selectedTechnologies.includes(technology)) {
+      this.selectedTechnologies = this.selectedTechnologies.filter(
+        (tech) => tech !== technology
+      );
+    } else {
+      this.selectedTechnologies.push(technology);
+    }
+  }
+
+  isSelected(technology: Technology): boolean {
+    return this.selectedTechnologies.includes(technology);
+  }
+
+  deselectAll(): void {
+    this.selectedTechnologies = [];
   }
 }
