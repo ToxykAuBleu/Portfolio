34bdc94fe1f5a53e9ad10017882f5683e650289b
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-02-20 20:51:19 +0100

    ✨ Liste des technologies utilisées dans le projet flexible et dynamique.

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Project } from '@models/project.model';
 import { IconTechComponent } from '../ui/icon-tech/icon-tech.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { Technology } from '@models/technology.model';
+import { debounceTime, fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-project',
@@ -19,24 +28,63 @@ import { Technology } from '@models/technology.model';
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss',
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit, AfterViewInit {
+  @ViewChild('techContainer') techContainer!: ElementRef;
   @Input() project!: Project;
   slicedTechs: Technology[] = [];
-
-  readonly maxTechs = 9;
+  overflowCount = 0;
+  resizeSubscription!: Subscription;
 
   get shortDescription(): string {
     return this.project.Description.split('.').slice(0, 1) + '.';
   }
 
-  get numberOfOtherTechs(): number {
-    return this.project.Technologies.length - (this.maxTechs - 1);
-  }
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.slicedTechs = this.project.Technologies.slice(0, this.maxTechs - 1);
-    if (this.project.Technologies.length >= this.maxTechs) {
+    this.slicedTechs = this.project.Technologies;
+
+    // Subscribe to window resize events to check if the technologies overflow the container.
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(50))
+      .subscribe(() => this.checkOverflow());
+  }
+
+  ngAfterViewInit() {
+    this.checkOverflow();
+  }
+
+  /**
+   * Check if the technologies overflow the container and slice them if necessary.
+   */
+  checkOverflow() {
+    const container = this.techContainer.nativeElement;
+    const children = container.children;
+    let numChildren = children.length;
+    let totalWidth = 0;
+    if (this.overflowCount > 0) {
+      numChildren += this.overflowCount;
+    } else {
+      numChildren++;
+    }
+
+    this.overflowCount = 0;
+
+    for (let i = 0; i < numChildren; i++) {
+      totalWidth += children[1].offsetWidth + 12;
+      if (totalWidth > container.offsetWidth) {
+        this.overflowCount = numChildren - i;
+        break;
+      }
+    }
+
+    this.slicedTechs = this.project.Technologies.slice(
+      0,
+      this.project.Technologies.length - this.overflowCount
+    );
+    if (this.overflowCount > 0) {
       this.slicedTechs.push(new Technology('...', '', '', ''));
     }
+    this.cdr.detectChanges();
   }
 }
