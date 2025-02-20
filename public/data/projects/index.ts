1b8c28fde1071929de0ba828a90b3a5f31f5b129
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-02-20 07:34:12 +0100

    ✨ Ajout de 3 nouveaux projets: AlgoForge, Lecteur-diaporama et VenteCd.

--- public/data/projects/index.ts
+++ public/data/projects/index.ts
@@ -1,3 +1,6 @@
 import Pixees from './pixees.project.json';
+import Algoforge from './algoforge.project.json';
+import VenteCD from './vente-cd.project.json';
+import LecteurDiaporama from './lecteur-diaporama.project.json';
 
-export const Projects: any[] = [Pixees, Pixees, Pixees, Pixees];
+export const Projects: any[] = [Algoforge, Pixees, VenteCD, LecteurDiaporama];
