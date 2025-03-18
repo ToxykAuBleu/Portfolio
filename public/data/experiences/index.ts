3511bbe6d879fa5222a23905772a1c171a61dadf
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-03-18 11:35:26 +0100

    ✏️ Mise à jour des expériences.

--- public/data/experiences/index.ts
+++ public/data/experiences/index.ts
@@ -3,7 +3,9 @@ import Cambridge from "./cambridge.exp.json";
 import BAC from "./bac.exp.json";
 import NuitInfo22 from "./nuit-info-22.exp.json";
 import NuitInfo23 from "./nuit-info-23.exp.json";
+import NuitInfo24 from "./nuit-info-24.exp.json";
 import SafranStage from "./safran-stage.exp.json";
+import SafranAlternance from "./safran-alternance.exp.json";
 
 export const Experiences: any[] = [
   Lineria,
@@ -12,4 +14,6 @@ export const Experiences: any[] = [
   NuitInfo22,
   NuitInfo23,
   SafranStage,
+  NuitInfo24,
+  SafranAlternance,
 ];
