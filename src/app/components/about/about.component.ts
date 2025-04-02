f35bd9ecda8a3de798a281c5a3dc6fbd6cc4cd5e
ToxykAuBleu <46644925+ToxykAuBleu@users.noreply.github.com>
2025-04-02 13:57:18 +0000

    ✨ Début de l'écriture de la page A propos de moi.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ImageLoaderComponent } from "@components/ui/image-loader/image-loader.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: "app-about",
-  imports: [],
+  imports: [ImageLoaderComponent, FontAwesomeModule, RouterModule],
   templateUrl: "./about.component.html",
   styleUrl: "./about.component.scss",
 })
