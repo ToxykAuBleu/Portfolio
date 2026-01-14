7f618b5829dd8ee20ff5fba1eae4d280f23dd266
ToxykAuBleu <github@toxykaubleu.fr>
2026-01-14 12:40:16 +0100

    🔧 Modification de l'image Docker: le build est réalisé dedans.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,32 @@
-FROM oven/bun:1.3.5-alpine
+# ------------------------------------------------------------------------------
+# BUILD APPLICATION INSIDE CONTAINER
+# ------------------------------------------------------------------------------ 
+FROM oven/bun:1.3.6-alpine AS builder
+WORKDIR /app
+# Install dependencies.
+COPY package.json bun.lock ./
+RUN bun install --frozen-lockfile
+
+# Build application.
+# NOTE: For the time being, bun can't build alone the app due to this angular error:
+# [ERROR] An error occurred while extracting routes. Cannot find module 'memory:///server.mjs' from '/app/node_modules/@angular/build/src/utils/load-esm.js'
+RUN apk add --no-cache nodejs
+COPY . ./
+RUN node ./node_modules/@angular/cli/bin/ng.js build
+
+# ------------------------------------------------------------------------------
+# USING BUILT CONTAINER
+# ------------------------------------------------------------------------------
+FROM oven/bun:1.3.6-alpine AS runtime
 LABEL org.opencontainers.image.version="2.0.0"
 LABEL org.opencontainers.image.title="Portfolio"
 LABEL org.opencontainers.image.source="https://github.com/ToxykAuBleu/Portfolio"
-
 WORKDIR /app
-COPY dist ./dist
-COPY package.json ./
+
+COPY package.json bun.lock ./
+COPY --from=builder /app/dist ./dist
 
 ENV HOST=0.0.0.0
 ENV PORT=4000
+EXPOSE 4000
 CMD ["bun", "run", "server"]
--- docker-build-image.sh
+++ docker-build-image.sh
@@ -6,49 +6,7 @@ IMAGE_TAG="latest"
 DOCKERFILE_PATH="Dockerfile"
 APP_DIR="."
 
-# Function to handle errors and exit.
-error_exit() {
-    echo "❌ $1"
-    exit 1
-}
-
-# Building the application if -b flag is provided.
-if [[ "$1" == "-b" ]]; then
-    echo "🚀 Building the application with Bun..."
-    ~/.bun/bin/bun ci --frozen-lockfile
-    ~/.bun/bin/bun run build
-    BUILD_STATUS=$?
-
-    if [ $BUILD_STATUS -ne 0 ]; then
-        echo "⚠️ Bun build failed. Trying fallback with Node + Angular CLI..."
-
-        # Check for Node.js using NVM.
-        if [ -d "$HOME/.nvm" ]; then
-            # Find the latest Node version installed via NVM.
-            NODE_DIR=$(ls -d $HOME/.nvm/versions/node/v*/bin 2>/dev/null | sort -V | tail -n 1)
-
-            if [ -n "$NODE_DIR" ] && [ -x "$NODE_DIR/node" ]; then
-                echo "🟢 Using Node from: $NODE_DIR"
-                "$NODE_DIR/node" node_modules/@angular/cli/bin/ng build
-                BUILD_STATUS=$?
-            else
-                error_exit "No valid Node version found under ~/.nvm/versions/node. Build failed."
-            fi
-        else
-            error_exit "NVM not found on this system. Build failed."
-        fi
-
-        if [ $BUILD_STATUS -ne 0 ]; then
-            error_exit "Fallback build with Node failed. Exiting."
-        else
-            echo "✅ Application built successfully with Angular CLI."
-        fi
-    else
-        echo "✅ Application built successfully with Bun."
-    fi
-fi
-
 # Building the Docker image.
 echo "🐳 Building Docker image..."
-docker build -t $IMAGE_NAME:$IMAGE_TAG -f $DOCKERFILE_PATH $APP_DIR || error_exit "Docker build failed."
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f $DOCKERFILE_PATH $APP_DIR || echo "❌ Docker build failed."
 echo "✅ Docker image $IMAGE_NAME:$IMAGE_TAG built successfully."
