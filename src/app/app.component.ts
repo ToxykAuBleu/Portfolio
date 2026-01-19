6e870f6c9de57cc1d800c7d299b5891da07f3917
ToxykAuBleu <github@toxykaubleu.fr>
2026-01-19 16:42:39 +0100

    ✨ Ajout des styles css de base. ✨ Ajout de la section "Hero".

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,112 +1,20 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import {
   FaIconLibrary,
   FontAwesomeModule,
 } from "@fortawesome/angular-fontawesome";
-import {
-  faAnglesDown,
-  faArrowDown19,
-  faArrowPointer,
-  faArrowUp19,
-  faAt,
-  faChevronDown,
-  faChevronUp,
-  faCircleInfo,
-  faCircleQuestion,
-  faCircleXmark,
-  faClock,
-  faClockRotateLeft,
-  faClose,
-  faDownload,
-  faEarListen,
-  faEllipsis,
-  faEnvelope,
-  faFileCode,
-  faFilterCircleXmark,
-  faFlagCheckered,
-  faFolderOpen,
-  faGears,
-  faGlobe,
-  faHandshakeAngle,
-  faHouseUser,
-  faLeaf,
-  faList,
-  faMapLocationDot,
-  faObjectGroup,
-  faPeopleGroup,
-  faQuoteLeft,
-  faQuoteRight,
-  faSeedling,
-  faToolbox,
-  faTriangleExclamation,
-  faUpRightFromSquare,
-  faUserClock,
-  faVial,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  faDiscord,
-  faGithub,
-  faLinkedin,
-  faSteam,
-} from "@fortawesome/free-brands-svg-icons";
-import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
 
 @Component({
   selector: "app-root",
   imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
-  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = "Portfolio";
 
   constructor(library: FaIconLibrary) {
-    library.addIcons(
-      faClose,
-      faClock,
-      faToolbox,
-      faEllipsis,
-      faList,
-      faFilterCircleXmark,
-      faGithub,
-      faMapLocationDot,
-      faGlobe,
-      faFlagCheckered,
-      faUpRightFromSquare,
-      faUserClock,
-      faGears,
-      faPeopleGroup,
-      faEarListen,
-      faHandshakeAngle,
-      faChevronUp,
-      faChevronDown,
-      faObjectGroup,
-      faArrowPointer,
-      faDownload,
-      faHouseUser,
-      faVial,
-      faFolderOpen,
-      faCircleQuestion,
-      faLinkedin,
-      faDiscord,
-      faSteam,
-      faLeaf,
-      faQuoteLeft,
-      faQuoteRight,
-      faCircleXmark,
-      faCircleInfo,
-      faFileCode,
-      faAnglesDown,
-      faSeedling,
-      faArrowUp19,
-      faArrowDown19,
-      faAt,
-      faEnvelope,
-      faXmarkCircle,
-      faClockRotateLeft,
-      faTriangleExclamation,
-    );
+    library.addIcons();
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { HeroComponent } from "@components/hero/hero.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: "app-home",
-  imports: [CommonModule, FontAwesomeModule, RouterModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule, HeroComponent],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
